84fa826ed204da0f1ec70200ccec5df0a1742dec
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-05-21 17:43:03 -0700

    Work in Progress

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,28 @@
+def read_template(string):
+    with open(string, 'r') as f:
+        content = f.read()
+        return content
 
-def read_template(txt):
+
+def parse_template(string):
+    import re
+
+    text_in_curly = (re.findall('{(.+?)}', string))
+
+    words_extract = set(text_in_curly)
+
+    new_string = string
+    for word in words_extract:
+        new_string = new_string.replace(word, "")
+
+    if new_string != string:
+        return '"'+new_string+'"', tuple(text_in_curly)
+
+    # stripped = '"'+new_string+'"'
+    # parts = tuple(text_in_curly)
+
+    # print(_stripped, _parts)
+
+
+def merge(string):
     pass
