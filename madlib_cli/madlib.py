4feda09cadbeb13ab33dfac82c9a9d7fd8021257
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-05-23 14:09:40 -0700

    Got three tests running

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,9 @@
+"""
+In this game we complete a story using the words you share. We only share the story after you give us
+the words to complete it. Since you don’t know what the story’s about, the result is a funny story.
+"""
+
+
 def read_template(string):
     with open(string, 'r') as f:
         content = f.read()
@@ -16,13 +22,24 @@ def parse_template(string):
         new_string = new_string.replace(word, "")
 
     if new_string != string:
-        return '"'+new_string+'"', tuple(text_in_curly)
-
-    # stripped = '"'+new_string+'"'
-    # parts = tuple(text_in_curly)
-
-    # print(_stripped, _parts)
+        return new_string, tuple(text_in_curly)
 
 
-def merge(string):
-    pass
+def merge(a, b):
+    import re
+
+    words = a.split()
+    i = 0; c = 0
+    while i < len(words):
+        if re.search("{}", words[i]) and c < len(b):
+            words[i] = words[i].replace('{}', b[c])
+            i += 1
+            c += 1
+        elif c < len(b):
+            i += 1
+        else:
+            break
+
+    if words != a:
+        new_string2 = ' '.join(words)
+        return new_string2
